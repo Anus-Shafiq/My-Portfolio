d681319b5800716f984a3a73af003fa9573c26cf
Anus Shafiq <sheikhanus925@gmail.com>
2025-01-17 00:47:52 +0500

    Add condition on form
    add regex for form so user cant be left them empty

--- form.js
+++ form.js
@@ -1,27 +1,64 @@
-let name = document.getElementById("name");
-let email = document.getElementById("email");
-let message = document.getElementById("message");
+// variables
+
+let userName = document.getElementById("name");
+let userEmail = document.getElementById("email");
+let userMessage = document.getElementById("message");
 let submitBtn = document.getElementById("submitBtn");
 let btnText = document.getElementById("btnText");
 let spinner = document.getElementById("spinner");
 
+// Regex
+
+var emailRegex = /^\S+@\S+\.\S+$/;
+
+var nameRegex = /^[A-Za-z]+(?: [A-Za-z]+)?$/;
+var messageRegex = /^\w+.*$/;
+
+// functions
+
 async function addForm() {
-  try {
-    btnText.classList.add("d-none");
-    spinner.classList.remove("d-none");
-    const { error } = await supabase
-      .from("Form_User")
-      .insert({ Name: name.value, Email: email.value, Message: message.value });
-    if (error) throw error;
-    name.value = "";
-    email.value = "";
-    message.value = "";
-  } catch (error) {
-    console.log(error);
-  } finally {
-    btnText.classList.remove("d-none");
-    spinner.classList.add("d-none");
-    btnText.innerText = "Thank You!";
+  if (nameRegex.test(userName.value)) {
+    if (emailRegex.test(userEmail.value)) {
+      if (messageRegex.test(userMessage.value)) {
+        try {
+          btnText.classList.add("d-none");
+          spinner.classList.remove("d-none");
+          const { error } = await supabase.from("Form_User").insert({
+            Name: userName.value,
+            Email: userEmail.value,
+            Message: userMessage.value,
+          });
+          if (error) throw error;
+          userName.value = "";
+          userEmail.value = "";
+          userMessage.value = "";
+        } catch (error) {
+          console.log(error);
+        } finally {
+          btnText.classList.remove("d-none");
+          spinner.classList.add("d-none");
+          btnText.innerText = "Thank You!";
+        }
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Empty Message Box",
+          text: "Please enter your message",
+        });
+      }
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Wrong Email",
+        text: "Please Enter Valid Email",
+      });
+    }
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Oops",
+      text: "Please enter your name",
+    });
   }
 }
 
